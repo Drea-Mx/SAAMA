20ce92ec636e2912ec6e890cb31bc13be6827110
Luis B <luisbttf@gmail.com>
2023-01-26 13:49:49 -0600

    netlify forms

--- gatsby/src/components/cotizador/modales/Depa.js
+++ gatsby/src/components/cotizador/modales/Depa.js
@@ -59,7 +59,7 @@ const Depa = ({depaInfo, depa01, setDepa01}) => {
                         </div>
                         <div className='bot'>
                             <p>Si te interesa este departamento envía un mensaje desde nuestro formulario y uno de nuestros asesores se pondrá en contacto contigo:</p>
-                            <form>
+                            <form name='depas' method="POST" netlify>
                                 <input type="hidden"  placeholder='Nombre' name='nombreDepa' value={depaInfo.codigoDepa} />
                                 <input type='text' placeholder='Nombre' name='nombre' />
                                 <input type='email' placeholder='Correo electrónico' name='email' />
