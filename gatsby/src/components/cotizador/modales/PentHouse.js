b05ed960259e4ed6f3c27d47cee26ed0e1a3679b
Luis B <luisbttf@gmail.com>
2023-01-30 10:38:21 -0600

    form

--- gatsby/src/components/cotizador/modales/PentHouse.js
+++ gatsby/src/components/cotizador/modales/PentHouse.js
@@ -95,7 +95,7 @@ const PentHouse = ({depaInfo, depa01, setDepa01}) => {
                         </div>
                         <div className='bot'>
                             <p>Si te interesa este departamento envía un mensaje desde nuestro formulario y uno de nuestros asesores se pondrá en contacto contigo:</p>
-                            <form>
+                            <form  name='penthouse' method="POST" data-netlify="true">
                                 <input type="hidden"  placeholder='Nombre' name='nombreDepa' value={depaInfo.codigoDepa} />
                                 <input type='text' placeholder='Nombre' name='nombre' />
                                 <input type='email' placeholder='Correo electrónico' name='email' />
